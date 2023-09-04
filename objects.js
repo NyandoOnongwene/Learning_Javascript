1320baf0a2446fa75e2754c996f57da999d6a115
Nyando Onongwene <nyandoonotex@gmail.com>
2023-09-04 12:50:06 +0100

    odject examples

---
+++ objects.js
@@ -0,0 +1,24 @@
+// const person = { name: 'John', age: 30, hobbies: ['reading', 'music'], greet: 
+// function() { 
+//     console.log('Hello!');
+//  } 
+// };
+// console.log(person.name);
+
+//  console.log(person.hobbies[0]);
+//  person.greet();
+
+let person = { name: 'John',
+age: 30, 
+address: { 
+street: '123 Main St', city: 'Anytown', state: 'CA' 
+}
+ }; 
+// Accessing object properties
+ let name = person.name;
+ let city = person.address.city; 
+// Modifying object properties
+ person.age = 40;
+ person.address.state = 'NY';
+
+ console.log( person.address.state);
