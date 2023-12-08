a999d99a70ba0d0472fd590288d90cfbec470087
Nyando Onongwene <nyandoonotex@gmail.com>
2023-12-08 10:05:34 +0100

    Learning Javascript in 3 weeks.

---
+++ Other/examples.js
@@ -0,0 +1,32 @@
+// function example() { 
+// var x = 1;
+// if (true) { 
+// var x = 2; 
+// }
+// console.log(x); 
+// } 
+// example();
+
+
+//Example 2
+// var x = 5; 
+// if (x = 10) { 
+// console.log('x is 10'); 
+// }
+// else {
+// console.log('x is not 10');
+// }
+
+
+let  a = 8;
+            if(a < 8)
+            {
+               document.write("Number is less than 8");
+            }
+            else if (a = 8)
+            {
+                document.write("Number is equal to 8");
+            }
+            else {
+                document.write("Number is greater than 8");
+            }
---
+++ Week 1/Day 1/into.js
@@ -0,0 +1,3 @@
+console.log("Hello World!!");
+
+console.log("I just started My day 1 of learning Javascript");
