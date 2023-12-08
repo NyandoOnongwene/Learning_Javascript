b62397e7dfc94eeda94bda9d42a5f06de6d6fa7b
Nyando Onongwene <nyandoonotex@gmail.com>
2023-12-08 10:36:13 +0100

    how to add quote "" s in strings

--- Week 1/Day 1/into.js
+++ Week 1/Day 1/into.js
@@ -1,3 +1,10 @@
 console.log("Hello World!!");
 
 console.log("I just started My day 1 of learning Javascript");
+
+// Alert
+// alert("Coding is fun!"); //This alerts on the web page, with a pop message at the top "Coding is fun" 
+
+console.log("John said that he loved icecream");
+// To add quotes include a \ before the ""
+console.log("John said \"I love icecream\"");
