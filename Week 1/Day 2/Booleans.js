e269f181a8cdea9938a0bc9ad075c40cc8a89320
Nyando Onongwene <nyandoonotex@gmail.com>
2023-12-09 10:23:25 +0100

    Day 2 boolean and for loop

---
+++ Week 1/Day 2/Booleans.js
@@ -0,0 +1,12 @@
+// let Variable = 1>10 ;
+// console.log(Variable);
+
+let number1 = 1 < 10 // Is 1 less than 10?
+let number2 = 100 > 2000 // Is 100 greater than 2000?
+let number3 = 2 === 2 // Is 2 exactly equal to 2?
+let number4 = false === false // Is false exactly equal to false?
+let number5 = 40 >= 40 // Is 40 greater than or equal to 40?
+
+console.log(number1, number2, number3, number4, number5);
+
+
